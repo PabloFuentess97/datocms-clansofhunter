f2bdc1791d9224c2aadaf696f42106c0aa987bf7
ClansOfHunter <pablofuentesoler97@gmail.com>
2021-08-11 15:50:00 +0200

    V 0.1.7.0.0.9

--- pages/index.js
+++ pages/index.js
@@ -111,54 +111,57 @@ export default function Home({ subscription }) {
       
       
 
-<div className="max-w-screen-sm mx-auto my-12">
+
+
+
+
+      <div className="max-w-screen-sm mx-auto my-12">
         {data && (
           <TransitionGroup>
-            {data.pumps.map((pump) => (
+            {data.posts.map((post) => (
               <CSSTransition
-                key={pump.id}
+                key={post.id}
                 classNames={{
-                  enter: "pump-enter",
-                  enterActive: "pump-enter-active",
-                  exit: "pump-exit",
-                  exitActive: "pump-exit-active",
+                  enter: "post-enter",
+                  enterActive: "post-enter-active",
+                  exit: "post-exit",
+                  exitActive: "post-exit-active",
                 }}
                 timeout={{ enter: 1200, exit: 1200 }}
               >
                 <div>
                   <div className="shadow-xl rounded-lg overflow-hidden bg-white">
-                    {pump.photos.map((photo) => (
+                    {post.photos.map((photo) => (
                       <Image
                         key={photo.responsiveImage.src}
                         className="w-full"
                         data={photo.responsiveImage}
                       />
                     ))}
-                    {pump.name && (
+                    {post.content && (
                       <div className="p-4 md:p-8 md:text-xl content">
-                        <ReactMarkdown children={pump.name} />
+                        <ReactMarkdown children={post.content} />
                       </div>
                     )}
                   </div>
-                  
+                  <div className="mt-4 grid grid-cols-2 text-xs md:text-sm text-gray-500 md:px-8 items-center pb-12">
+                    <div className="flex items-center">
+                      <Image
+                        className="w-6 h-6 rounded-full mr-2 shadow"
+                        data={post.author.avatar.responsiveImage}
+                      />
+                      <div>{post.author.name}</div>
+                    </div>
+                    <div className="text-right">
+                      <TimeAgo date={post._firstPublishedAt} />
+                    </div>
+                  </div>
                 </div>
               </CSSTransition>
             ))}
           </TransitionGroup>
         )}
       </div>
-
-
-
-
-
-
-
-
-
-
-
-      
     </div>
   );
 }
