176b15477ad23edd4e80b41e9b0baa3c951cf19b
ClansOfHunter <pablofuentesoler97@gmail.com>
2021-08-11 15:26:27 +0200

    V 0.1.7.0.0.6

--- pages/index.js
+++ pages/index.js
@@ -108,6 +108,67 @@ export default function Home({ subscription }) {
         </div>
       )}
 
+      
+      
+
+<div className="max-w-screen-sm mx-auto my-12">
+        {data && (
+          <TransitionGroup>
+            {data.posts.map((post) => (
+              <CSSTransition
+                key={post.id}
+                classNames={{
+                  enter: "post-enter",
+                  enterActive: "post-enter-active",
+                  exit: "post-exit",
+                  exitActive: "post-exit-active",
+                }}
+                timeout={{ enter: 1200, exit: 1200 }}
+              >
+                <div>
+                  <div className="shadow-xl rounded-lg overflow-hidden bg-white">
+                    {post.photos.map((photo) => (
+                      <Image
+                        key={photo.responsiveImage.src}
+                        className="w-full"
+                        data={photo.responsiveImage}
+                      />
+                    ))}
+                    {post.content && (
+                      <div className="p-4 md:p-8 md:text-xl content">
+                        <ReactMarkdown children={post.content} />
+                      </div>
+                    )}
+                  </div>
+                  <div className="mt-4 grid grid-cols-2 text-xs md:text-sm text-gray-500 md:px-8 items-center pb-12">
+                    <div className="flex items-center">
+                      <Image
+                        className="w-6 h-6 rounded-full mr-2 shadow"
+                        data={post.author.avatar.responsiveImage}
+                      />
+                      <div>{post.author.name}</div>
+                    </div>
+                    <div className="text-right">
+                      <TimeAgo date={post._firstPublishedAt} />
+                    </div>
+                  </div>
+                </div>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        )}
+      </div>
+
+
+
+
+
+
+
+
+
+
+
       <div className="max-w-screen-sm mx-auto my-12">
         {data && (
           <TransitionGroup>
