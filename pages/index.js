76e209b22179e948493c7687ce7298b2d5a4f148
ClansOfHunter <pablofuentesoler97@gmail.com>
2021-08-11 02:10:01 +0200

    V 0.1.0

--- pages/index.js
+++ pages/index.js
@@ -61,33 +61,32 @@ export default function Home({ subscription }) {
   return (
     <div className="text-gray-700 body-font py-12 bg-gray-100 px-10">
       <Head>
-        <title>Create Next App</title>
+        <title>ClansOfHunter | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="max-w-screen-sm mx-auto text-center">
         <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
-          Real-times Updates Demo
+          Real-times Updates 
         </p>
         <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-10">
-          Event Coverage LiveBlog
+          ClansOfHunter
         </h3>
         <p className="mt-4 max-w-xl text-xl leading-7 text-gray-500 lg:mx-auto">
-          A simple Next.js + Typescript + Tailwind project to demonstrate
-          real-time capabilities of DatoCMS
+          Pump a random coin chosen by the server, no one will know the coin before 
         </p>
       </div>
 
       <div className="max-w-screen-sm mx-auto text-center mt-20 mb-12">
         {status === "connecting" ? (
-          <div>Connecting to DatoCMS...</div>
+          <div>Connecting to ClansOfHunter...</div>
         ) : status === "connected" ? (
           <div className="flex flex-col md:flex-row items-center justify-center">
             <span className="flex h-3 w-3 relative mb-3 md:mb-0 md:mr-2">
               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-pink-400 opacity-75"></span>
               <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
             </span>
-            <span>Connected to DatoCMS, receiving live updates!</span>
+            <span>Connected to ClansOfHunter, receiving live updates!</span>
           </div>
         ) : (
           <div>Connection closed</div>
